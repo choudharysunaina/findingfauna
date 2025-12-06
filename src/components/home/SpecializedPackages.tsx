7382e74c7b9af6ebf3c6b6ad4501a460211c6f39
choudharysunaina <choudhary.sunaina09@gmail.com>
2025-12-06 23:41:04 +0530

    Fix handleClick undefined error in SpecializedPackages component

--- src/components/home/SpecializedPackages.tsx
+++ src/components/home/SpecializedPackages.tsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import ResponsiveImage from '../ui/ResponsiveImage';
 import { useRef } from 'react';
 
+const handleClick = () => {
+  window.scrollTo(0, 0);
+};
+
 interface PackageCardProps {
   imageUrl: string;
   title: string;
